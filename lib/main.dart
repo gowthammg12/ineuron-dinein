979766e4275046e91434fddda125123b54e9a8e8
gowthammg12 <gowtham.g@uni.club>
2022-06-11 16:00:43 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() async {
+  runApp(const DineInApp());
+}
+
+class DineInApp extends StatelessWidget {
+  const DineInApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
