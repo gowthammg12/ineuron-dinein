b52f553336406a33208a953950931bed8dbee994
kanish2211 <jskanishka@gmail.com>
2022-06-12 11:11:29 +0530

    order history page download button added

--- lib/screens/order_history_screen.dart
+++ lib/screens/order_history_screen.dart
@@ -57,28 +57,47 @@ class OrderHistoryScreen extends StatelessWidget {
         width: double.infinity,
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20.0),
-          child: Table(
+          child: Column(
             children: [
-              TableRow(
+              Table(
                 children: [
-                  Text("Item"),
-                  Text("Quantity"),
-                  Text("Cost"),
+                  TableRow(
+                    children: [
+                      Text("Item"),
+                      Text("Quantity"),
+                      Text("Cost"),
+                    ],
+                  ),
+                  ...(orderHistory.items ?? [])
+                      .map((e) => TableRow(
+                            children: [
+                              Text(e.name),
+                              Text("${e.quantity ?? 0}"),
+                              Text("${e.price * (e.quantity ?? 0)}"),
+                            ],
+                          ))
+                      .toList(),
                 ],
               ),
-              ...(orderHistory.items ?? [])
-                  .map((e) => TableRow(
-                        children: [
-                          Text(e.name),
-                          Text("${e.quantity ?? 0}"),
-                          Text("${e.price * (e.quantity ?? 0)}"),
-                        ],
-                      ))
-                  .toList(),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                                padding: const EdgeInsets.only(left: 20.0, right: 20,top: 40
+                                ),
+                                child: OutlinedButton(
+                                  child: Text(
+                                    "Download bill",
+                                    style: TextStyle(color: Colors.black54),
+                                  ),
+                                  onPressed: () {},
+                                ),
+                              ),
+              ),
             ],
           ),
         ),
       ),
+      
     );
   }
 }
